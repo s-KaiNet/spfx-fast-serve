f35c0000b93a6359babac64def3b5170db310f37
Sergei Sergeev <sergeev.srg@gmail.com>
2021-04-02 23:02:01 +0300

    Fixed schema url.

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -1,4 +1,4 @@
 export const FastServeFolderName = 'fast-serve';
 export const ConfigFileName = 'config.json';
-export const SchemaUrl = 'https://github.com/s-KaiNet/spfx-fast-serve/blob/master/schema/config.1.0.schema.json';
+export const SchemaUrl = 'https://raw.githubusercontent.com/s-KaiNet/spfx-fast-serve/master/schema/config.1.0.schema.json';
 export const WebpackExtendFileName = 'webpack.extend.js';
