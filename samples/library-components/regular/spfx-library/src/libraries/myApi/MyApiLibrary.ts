05de3dadd0adf0e795b0a300287a6ac59e38a2aa
Sergei Sergeev <sergeev.srg@gmail.com>
2021-05-10 13:45:21 +0300

    Upgraded library components to the latest version.

--- samples/library-components/regular/spfx-library/src/libraries/myApi/MyApiLibrary.ts
+++ samples/library-components/regular/spfx-library/src/libraries/myApi/MyApiLibrary.ts
@@ -2,7 +2,6 @@ import * as strings from 'MyApiLibraryStrings';
 
 export class MyApiLibrary {
   public name(): string {
-    return strings.PropertyPaneDescription ;
-    //return "hello";
+    return strings.PropertyPaneDescription;
   }
 }
