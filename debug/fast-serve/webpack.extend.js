5116c0fdde8cd12121711193bcb3752824a18110
Sergei Sergeev <sergeev.srg@gmail.com>
2021-10-23 18:05:52 +0300

    Removed unused folder.

--- debug/fast-serve/webpack.extend.js
+++
@@ -1,9 +0,0 @@
-/*
-* User webpack settings file. You can add your own settings here.
-* Changes from this file will be merged into the base webpack configuration file.
-* This file will not be overwritten by the subsequent spfx-fast-serve calls.
-*/
-
-module.exports = {
-
-}
